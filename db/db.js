0832e0ead41eee61d899c586175c00f39f2a649e
pranav <pranav@intimetec.com>
2021-08-05 12:58:02 +0530

    db config changed

--- db/db.js
+++ db/db.js
@@ -1,16 +1,22 @@
-const mysql = require('mysql');
-const util = require('util');
+const mysql = require("mysql");
+const util = require("util");
+
+// const connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'root',
+//     database : 'create_vin'
+//   });
 
 const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'create_vin'
-  });
+  host: "localhost",
+  user: "root",
+  password: "Qalladze.99#!",
+  database: "create_vin",
+});
 
 const query = util.promisify(connection.query).bind(connection);
 
 module.exports = {
-    query,
-}
-
+  query,
+};
